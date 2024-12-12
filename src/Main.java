202b14f28e8417b9c744468026662a40159b7572
shubhamieshubhism <shubhamtirpude20@gmail.com>
2024-12-12 16:51:54 +0530

    correct the spelling of output

--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,9 @@ import leetCodeProgrammingPractice.longestPalindromicSubString;
 public class Main extends longestPalindromicSubString {
     public static void main(String[] args) {
         String input = "asaadaajk";
-        String outpur = longestPalindrome(input);
-        System.out.println(outpur);
+        String output = longestPalindrome(input);
+        System.out.println(output);
+
 
     }
 }
